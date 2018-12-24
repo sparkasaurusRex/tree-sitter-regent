847ea05e5eb314b6eb321403a10f98057388441d
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2018-12-24 01:54:01 -0600

    add test cases

--- grammar.js
+++ grammar.js
@@ -9,19 +9,73 @@ module.exports = grammar(require('tree-sitter-lua/grammar'), {
     )),
 
     _regent_statement: $ => choice(
+      // alias($.task_statement, $.task),
+      // alias($.terra_statement, $.terra),
       alias($.fspace_statement, $.fspace),
+      // alias($.struct_statement, $.struct),
+      // TODO: var x = ...
+      // $.copy_statement,
+      // $.fill_statement
     ),
+
+    // _regent_expression: $ => choice(
+    //   $.ispace,
+    //   $.region,
+    //   // Partition ops
+    //   // __demand
+    //   // __forbid
+    // ),
+
+    _regent_type: $ => choice(
+      $.regent_primitive_type,
+      $.region_type,
+      $.ptr_type,
+      // $.ispace_type,
+      alias($.identifier, $.type_identifier)
+    ),
+
+    region_type: $ => seq(
+      'region',
+      '(',
+      $._regent_type,
+      ')'
+    ),
+
+    ptr_type: $ => seq(
+      'ptr',
+      '(',
+      $._regent_type,
+      ',',
+      alias($.identifier, $.region_identifier), // TODO: Do I always need this?
+      ')'
+    ),
+
+    regent_primitive_type: $ => choice(
+      'bool',
+      ...['', 8, 16, 32].map(n => `int${n}`),
+      ...['', 8, 16, 32].map(n => `uint${n}`),
+      'float',
+      'double',
+      ...[1, 2, 3].map(n => `int${n}d`),
+      ...[1, 2, 3].map(n => `rect${n}d`)
+    ),
+
+    // task_statement: $ => seq(
+    //   'task',
+    //   alias($.identifier, $.task_name),
+    //   '(',
+    //   ')',
+    //   'return',
+    //   'true',
+    //   'end'
+    // ),
 
     fspace_statement: $ => seq(
       'fspace',
       alias($.identifier, $.fspace_name),
       optional(seq(
         '(',
-        commaSep(seq(
-          alias($.identifier, $.fspace_argument_name),
-          ':',
-          alias($.identifier, $.fspace_argument_type)
-        )),
+        commaSep($.fspace_argument),
         ')'
       )),
       '{',
@@ -29,11 +83,19 @@ module.exports = grammar(require('tree-sitter-lua/grammar'), {
       '}'
     ),
 
+    fspace_argument: $ => seq(
+      alias($.identifier, $.name),
+      ':',
+      $._regent_type
+    ),
+
     fspace_field: $ => seq(
       alias($.identifier, $.fspace_field_name),
       ':',
-      alias($.identifier, $.regent_type)
-    )
+      $._regent_type
+    ),
+
+    wild: $ => 'wild' // TODO: Figure out where to use
   }
 })
 
